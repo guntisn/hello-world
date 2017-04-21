b46d0ef3e9a9068be0d1c173e8bedfcc423ae84c
guntisn <guntisn@users.noreply.github.com>
2017-04-21 14:07:08 +0300

    Add files via upload

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Paldiesiòð</title>
+        <style>
+        </style>
+    </head>
+    <body>
+        <h1>Esi piereìistrçjies ar {{ $theEmail }}</h1>
+    </body>
+</html>
